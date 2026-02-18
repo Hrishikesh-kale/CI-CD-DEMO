056c728df523ac80d459b7b139cd8d5310cfb0d0
Hrushikesh Kale <84776928+Hrishikesh-kale@users.noreply.github.com>
2026-02-18 15:39:49 +0530

    Update TestController.java

--- demo-app-1/src/main/java/com/example/demo/TestController.java
+++ demo-app-1/src/main/java/com/example/demo/TestController.java
@@ -5,13 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-// test cmt asdfghjkl;
+// test cmt 
 	@GetMapping(value = "/get")
 	public String getTestData() {
 	
 		return "Hello from Spring Boot";
 	}
 }
+
 
 
 
